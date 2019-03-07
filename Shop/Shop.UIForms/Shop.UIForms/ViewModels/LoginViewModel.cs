21ac5508001835eb53238a080977a8bbeacdfb11
JOSE ALFREDO PONCIANO CARRANZA <mectoys2013@gmail.com>
2019-03-07 16:27:34 -0500

    Part 21 -LoginPage

---
+++ Shop/Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
@@ -0,0 +1,55 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Shop.UIForms.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+        public LoginViewModel()
+        {
+            this.Email = "mectoys2013@gmail.com";
+            this.Password = "123456";
+        }
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a Email",
+                    "Accept");
+                return;
+            }
+            if (!this.Email.Equals("mectoys2013@gmail.com") || !this.Password.Equals("123456"))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "User or password wrong.",
+                    "Accept");
+                return;
+            }
+
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a Password",
+                    "Accept");
+                return;
+            }
+        }
+    }
+}
